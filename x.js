f88f80598c949499ead5d1f51d3cdc2140a5c6e7
ItsTheWhale <103934114+ItsTheWhale@users.noreply.github.com>
2023-03-07 20:55:02 +0800

    Delete x.js

--- x.js
+++
@@ -1,8 +0,0 @@
-const {REST} = require('@discordjs/rest');
-const {Routes} = require('discord-api-types/v10');
-
-const rest = new REST({ version: '10' }).setToken("MTA4MTc4MTg2MzQxMDA2MTQxNQ.G1DOto.DiNWlehMZHdXznENNTcXJe_gH-qtjgjYCj_b2w");
-
-rest.put(Routes.applicationGuildCommands("1081781863410061415", "999435288478765211"), { body: [] })
-	.then(() => console.log('Successfully deleted all guild commands.'))
-	.catch(console.error);
